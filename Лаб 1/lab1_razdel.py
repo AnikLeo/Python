7097cc3b2e5a4e84b4b86911af2202a9c9d7b8fd
AnikLeo <anikeevleonid@gmail.com>
2024-10-19 08:40:56 +0300

    Лаб 1

---
+++ Лаб 1/lab1_razdel.py
@@ -0,0 +1,11 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+midl_ind=len(list_players)//2
+
+team1 = list_players[:midl_ind]
+team2 = list_players[midl_ind:]
+
+print(team1)
+print(team2)
+
+
